b71a12dbb4c761f1405ca4c59b9b8db0ff78dffe
MithraMugundan <116003147+MithraMugundan@users.noreply.github.com>
2025-04-19 11:26:46 +0530

    Delete JAVA CODING CHALLANGE/INSURANCE MANAGEMENT SYSTEM directory

--- JAVA CODING CHALLANGE/INSURANCE MANAGEMENT SYSTEM/src/com/hexaware/ims/exceptions/ClaimNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hexaware.ims.exceptions;
-
-public class ClaimNotFoundException extends Exception {
-    public ClaimNotFoundException(String message) {
-        super(message);
-    }
-}
